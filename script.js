9ec251e464fafcbcfb68fd5f23b54c5f625b6a89
Gafuroff-prog <gafuroffgaming@gmail.com>
2024-11-13 17:07:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('Sizda random son chiqadi va ohirida aytiladi');
+
+let number = Math.floor(Math.random ()*10)+1;
+
+alert('random chiqqan sonni 2 ga kopaytirsak shu son chiqadi ->');
+let number2 = number*2
+let number3 = number/2
+alert(`Sizga random chiqqan son edi ${number}, 2ga kopaytirilganda shu son chiqdi ${number2}, bolinganda ${number3}`);
